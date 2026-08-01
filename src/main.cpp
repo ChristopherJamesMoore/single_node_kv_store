1fbba684c5299b845c43ca24f2575206f5c8ca99
Chris Moore <90791015+ChristopherJamesMoore@users.noreply.github.com>
2026-08-01 23:54:56 +0200

    feat: extract key/value and branch on the command

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,23 @@ int main() {
     std::string cmd;
     iss >> cmd;
 
-    std::cout << "command was: " << cmd << "\n";
-    std::cout << "kvstore> ";
+    if (cmd == "SET") {
+      std::string key, value;
+      iss >> key >> value;
+      std::cout << "would SET " << key << " = " << value << "\n";
+    } else if (cmd == "GET") {
+      std::string key; 
+      iss >> key;
+      std::cout << "would GET " << "\n";
+    } else if (cmd == "DEL") {
+      std::string key;
+      iss >> key;
+      std::cout << "would DEL " << "\n";
+    } else if (cmd == "QUIT") {
+      break;
+    } else {
+      std::cout << "unkown command\n";
+    }
   }
 
   return 0;
