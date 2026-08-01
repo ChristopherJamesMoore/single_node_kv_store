d88a96418c0c2076cf24bc29df8f9c95312939f1
Chris Moore <90791015+ChristopherJamesMoore@users.noreply.github.com>
2026-08-01 22:52:32 +0200

    chore: project structuring

---
+++ build/CMakeFiles/kvstore.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/kvstore.dir/src/main.cpp.o: \
+  /Users/christophermoore/Documents/single_node_kv_store/src/main.cpp
---
+++ build/CMakeFiles/kvstore.dir/src/store.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/kvstore.dir/src/store.cpp.o: \
+  /Users/christophermoore/Documents/single_node_kv_store/src/store.cpp
