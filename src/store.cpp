9d258b2bfbf8792ee9384adf9bad8c9256c98c84
Chris Moore <90791015+ChristopherJamesMoore@users.noreply.github.com>
2026-08-02 18:56:14 +0200

    feat: implementing store methods

---
+++ src/store.cpp
@@ -0,0 +1,17 @@
+#include <store.hpp>
+
+void Store::set(const std::string& key, const std::string& key) {
+  data_[key] = value;
+}
+
+std::optional<std::string> Store::get(const std::string& key) const {
+  auto it = data_.find(key);
+  if (it == data_.end()) {
+    return std::nullpoint;
+  }
+  return it->second;
+}
+
+bool Store::remove(const std::string& key) {
+  return data_.erase(key) > 0;
+}
