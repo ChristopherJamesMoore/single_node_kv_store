5121dafa39893e8a0b224b10e8552a3d6767f410
Chris Moore <90791015+ChristopherJamesMoore@users.noreply.github.com>
2026-08-02 19:05:55 +0200

    bugfix: key swap to value & missing semicolon on storehpp

--- src/store.cpp
+++ src/store.cpp
@@ -1,13 +1,13 @@
 #include "store.hpp"
 
-void Store::set(const std::string& key, const std::string& key) {
+void Store::set(const std::string& key, const std::string& value) {
   data_[key] = value;
 }
 
 std::optional<std::string> Store::get(const std::string& key) const {
   auto it = data_.find(key);
   if (it == data_.end()) {
-    return std::nullpoint;
+    return std::nullopt;
   }
   return it->second;
 }
--- src/store.hpp
+++ src/store.hpp
@@ -5,7 +5,7 @@
 
 class Store {
   public:
-    void set(const std::string& key, const std::string& value)
+    void set(const std::string& key, const std::string& value);
     std::optional<std::string> get(const std::string& key) const;
     bool remove(const std::string& key);
 
